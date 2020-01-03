ccc7d0e0dc8c06b7329950c2c7e40e30d59e4d50
Christian Berndt <christian.berndt@liferay.com>
2020-01-03 16:45:25 +0100

    Preliminary DependencyResolver implementation.

---
+++ src/main/java/com/chberndt/liferay/site/initializer/MySiteInitializerDependencyResolver.java
@@ -0,0 +1,35 @@
+package com.chberndt.liferay.site.initializer;
+
+import com.liferay.portal.kernel.util.StringUtil;
+
+//import com.liferay.petra.string.StringUtil;
+//import com.liferay.portal.kernel.util.FileUtil;
+
+import java.io.IOException;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * 
+ * @author Christian Berndt
+ *
+ */
+@Component(
+	immediate=true,
+	service=MySiteInitializerDependencyResolver.class
+)
+public class MySiteInitializerDependencyResolver {
+	
+	public String getDependenciesPath() {
+		return _DEPENDENCIES_PATH;
+	}
+
+	public String getJSON(String name) throws IOException {
+		return StringUtil.read(MySiteInitializerDependencyResolver.class.getClassLoader(), 
+			_DEPENDENCIES_PATH + name);
+	}
+
+	private static final String _DEPENDENCIES_PATH =
+		"com/chberndt/liferay/site/initializer/dependencies/";
+
+}
