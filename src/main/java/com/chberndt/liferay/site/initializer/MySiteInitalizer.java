bdc606fb1883052188b43368b65b09ce8699e586
Christian Berndt <christian.berndt@liferay.com>
2020-01-07 11:29:01 +0100

    Add createRoles to initialize method.

--- src/main/java/com/chberndt/liferay/site/initializer/MySiteInitalizer.java
+++ src/main/java/com/chberndt/liferay/site/initializer/MySiteInitalizer.java
@@ -1,13 +1,19 @@
 package com.chberndt.liferay.site.initializer;
 
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
-import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.site.exception.InitializationException;
 import com.liferay.site.initializer.SiteInitializer;
 
@@ -52,6 +58,11 @@ public class MySiteInitalizer implements SiteInitializer {
 	public void initialize(long groupId) throws InitializationException {
 		try {
 			_log.info("MySiteInitializer.initialize()"); 
+			
+			ServiceContext serviceContext = getServiceContext(groupId);
+			
+			createRoles(serviceContext);
+
 		} catch (Exception e) {
 			_log.error(e, e);
 
@@ -65,10 +76,34 @@ public class MySiteInitalizer implements SiteInitializer {
 	}
 	
 	protected void createRoles(ServiceContext serviceContext) throws Exception {
+		
+		_log.info("MySiteInitializer.createRoles()"); 
+
 		JSONArray jsonArray = _getJSONArray("roles.json");
 
 		_myFileImporter.createRoles(jsonArray, serviceContext);
 
+	}
+	
+	protected ServiceContext getServiceContext(long groupId)
+			throws PortalException {
+
+		User user = _userLocalService.getUser(PrincipalThreadLocal.getUserId());
+		Group group = _groupLocalService.getGroup(groupId);
+
+		Locale locale = LocaleUtil.getSiteDefault();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setCompanyId(group.getCompanyId());
+		serviceContext.setLanguageId(_language.getLanguageId(locale));
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setTimeZone(user.getTimeZone());
+		serviceContext.setUserId(user.getUserId());
+
+		return serviceContext;
 	}
 	
 	private JSONArray _getJSONArray(String name) throws Exception {
@@ -81,22 +116,22 @@ public class MySiteInitalizer implements SiteInitializer {
 	private static final Log _log = LogFactoryUtil.getLog(MySiteInitalizer.class);
 	
 	@Reference
+	private JSONFactory _jsonFactory;
+	
+	@Reference
+	private Language _language; 
+	
+	@Reference
 	private MyFileImporter _myFileImporter;
 	
 	@Reference
-	private JSONFactory _jsonFactory;
-
-	@Reference
-	private ResourcePermissionLocalService _resourcePermissionLocalService;
-	
-
-	@Reference
-	private RoleLocalService _roleLocalService;
-	
-	@Reference(
-		target = "(site.initializer.key=" + MySiteInitalizer.KEY + ")"
-	)
 	private MySiteInitializerDependencyResolver
 		_siteInitializerDependencyResolver;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+	
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
